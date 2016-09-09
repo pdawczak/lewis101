a348e4af77e928291b611b962be0bbc1ba93ead5
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-09-09 10:32:36 +0100

    Add module with function for spawn

--- playground.ex
+++ playground.ex
@@ -3,3 +3,22 @@ greeter = fn ->
     name -> IO.puts "Hello, #{String.upcase(name)}"
   end
 end
+
+defmodule Play do
+  def greeter do
+    receive do
+      name -> IO.puts "Hello, #{String.upcase(name)}"
+    end
+
+    greeter
+  end
+end
+
+# iex(26)> pid = spawn(Play, :greeter, [])
+# #PID<0.116.0>
+# iex(27)> send pid, "Lewis"
+# Hello, LEWIS
+# "Lewis"
+# iex(28)> send pid, "Paweł"
+# Hello, PAWEŁ
+# "Paweł"
